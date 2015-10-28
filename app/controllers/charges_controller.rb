4451fdc48d6553b8b9719655b74043abb3df90d5
David Correa Orozco <davidcroz1@gmail.com>
2015-10-28 16:27:48 -0700

    Implemented subscription based product GROHACK2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,19 +1,17 @@
+# This is the code to create a recurrence subscription.
+
 class ChargesController < ApplicationController
   def create
-    product = Product.find_by_sku("GROHACK1")
+    product = Product.find_by_sku("GROHACK2")
 
     customer = Stripe::Customer.create(
       #We want to get the email from the form
       :email => params[:stripeEmail],
-      :card  => params[:stripeToken]
+      :card  => params[:stripeToken],
+      :plan  => "GROHACK2"
     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => product.price_in_cents,
-      :description => 'Growth Hacking Crash Course',
-      :currency    => 'usd'
-    )
+    # no need for the charge
 
     # Now we want to show the purchase
     # using amount is not a good idea. They could change the amount
@@ -21,7 +19,7 @@ class ChargesController < ApplicationController
     # This is saved in the database
     purchase = Purchase.create(email: params[:stripeEmail],
       card: params[:stripeToken], amount: product.price_in_cents,
-      description: charge.description, currency: charge.currency,
+      description: product.full_description, currency: 'usd',
       customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
     # This is a little bit of Rails magic.
@@ -36,3 +34,44 @@ class ChargesController < ApplicationController
   end
 
 end
+
+# The code below is for a single purchase
+
+# class ChargesController < ApplicationController
+#   def create
+#     product = Product.find_by_sku("GROHACK1")
+#
+#     customer = Stripe::Customer.create(
+#       #We want to get the email from the form
+#       :email => params[:stripeEmail],
+#       :card  => params[:stripeToken]
+#     )
+#
+#     charge = Stripe::Charge.create(
+#       :customer    => customer.id,
+#       :amount      => product.price_in_cents,
+#       :description => 'Growth Hacking Crash Course',
+#       :currency    => 'usd'
+#     )
+#
+#     # Now we want to show the purchase
+#     # using amount is not a good idea. They could change the amount
+#     # Not good practice to pass the amount you are charging in a form field
+#     # This is saved in the database
+#     purchase = Purchase.create(email: params[:stripeEmail],
+#       card: params[:stripeToken], amount: product.price_in_cents,
+#       description: charge.description, currency: charge.currency,
+#       customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+#
+#     # This is a little bit of Rails magic.
+#     # Redirect to show the purchase based upon
+#     # whatever the id of this new purchase we've created is.
+#     # I belive this is also the same as path_to(purchase)
+#     redirect_to purchase
+#
+#   rescue Stripe::CardError => e
+#     flash[:error] = e.message
+#     redirect_to charges_path
+#   end
+#
+# end
