b58f53549b1d4cd62dd9812c34ce50d36340ba3e
David Correa Orozco <davidcroz1@gmail.com>
2015-10-28 01:58:42 -0700

    Adding emailing of a purchase receipt

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,9 +1,16 @@
 class Purchase < ActiveRecord::Base
+  after_create :email_purchaser
   #Override to_param. This method is used when we
   # go and get a particular purchase and we try to get
   # that as a value, usually the to_param will return
   # the id. We are going to ask it to return a uuid instead.
   def to_param
     uuid
+  end
+  
+  private
+
+  def email_purchaser
+    PurchaseMailer.purchase_receipt(self).deliver
   end
 end
